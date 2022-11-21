69dd182d156bfd3b157344cc008db8d59673c5be
Gunwoo Kim <gunwoolouie@gmail.com>
2022-11-20 23:36:53 -0800

    done with part1, 2

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 8 - Starter
+### Question Answers
+1) I would put my automated tests **(1) within GitHub Actions** because we want the tests to run in our CI pipeline. When we push any code into our repo, it should run basic tests that ensures that all the parts of the code is exhibiting the desired behavior.
+
+2) **no**. Testing the smaller parts of the code is for the unit tests.
+
+3) **no**. I would use E2E testing. This is because the message feature isn't an isolated test on a small part of code or a function. The message function is probably an interaction between many parts of the code. So I would tests the act of sending a message to a user results in the other user recieveing the message, rather than checking if the code is doing the right thing. In other words, I'm testing the the "bigger picture" of sending a message, leading to that message being receieved, etc, i.e. end to end testing.
+
+4) **no**. It is specified that "max message length" is a feature. So it requires testing it in a higher lever. i.e. the tester should attempt to put in text longer than 80 characters and see if the entry box only accepts the first 80 characters that has been entered. 
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,81 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// TODO - Part 2 
+
+// isPhoneNumber
+test('isPhoneNumber("423-995-3882") is true', () => {
+    expect(functions.isPhoneNumber("423-995-3882")).toBe(true);
+})
+test('isPhoneNumber("894-3333-4323") is true', () => {
+    expect(functions.isPhoneNumber("894-3333-4323")).toBe(true);
+})
+test('isPhoneNumber("894-938-343") is false', () => {
+    expect(functions.isPhoneNumber("894-938-343")).toBe(false);
+})
+test('isPhoneNumber("894-93a18-343") is false', () => {
+    expect(functions.isPhoneNumber("894-93a18-343")).toBe(false);
+})
+
+
+// isEmail
+test('isEmail("anValidEmail@gmail.com") is true', () => {
+    expect(functions.isEmail("anValidEmail@gmail.com")).toBe(true);
+})
+test('isEmail("anothervalidemail@hotmail.net") is true', () => {
+    expect(functions.isEmail("anothervalidemail@hotmail.net")).toBe(true);
+})
+test('isEmail("invalidemail@nodomain") is false', () => {
+    expect(functions.isEmail("invalidemail@nodomain")).toBe(false);
+})
+test('isEmail("bad") is false', () => {
+    expect(functions.isEmail("bad")).toBe(false);
+})
+
+
+
+// isStrongPassword
+test('isStrongPassword("aStrongPassword") is true', () => {
+    expect(functions.isStrongPassword("aStrongPassword")).toBe(true);
+})
+test('isStrongPassword("anotherStrong123_") is true', () => {
+    expect(functions.isStrongPassword("anStrong123_")).toBe(true);
+})
+test('isStrongPassword("bad") is false', () => {
+    expect(functions.isStrongPassword("bad")).toBe(false);
+})
+test('isStrongPassword("1badone") is false', () => {
+    expect(functions.isStrongPassword("1badone")).toBe(false);
+})
+
+
+
+// isDate
+test('isDate("1/2/2022") is true', () => {
+    expect(functions.isDate("1/2/2022")).toBe(true);
+})
+test('isDate("01/2/1999") is true', () => {
+    expect(functions.isDate("01/2/1999")).toBe(true);
+})
+test('isDate("01/02/99") is false', () => {
+    expect(functions.isDate("01/02/99")).toBe(false);
+})
+test('isDate("feb 13") is false', () => {
+    expect(functions.isDate("feb 13")).toBe(false);
+})
+
+
+
+// isHexColor
+test('isHexColor("#a6b784") is true', () => {
+    expect(functions.isHexColor("#a6b784")).toBe(true);
+})
+test('isHexColor("#39f") is true', () => {
+    expect(functions.isHexColor("#39f")).toBe(true);
+})
+test('isHexColor("39x") is false', () => {
+    expect(functions.isHexColor("39x")).toBe(false);
+})
+test('isHexColor("somethingVeryBad") is false', () => {
+    expect(functions.isHexColor("somethingVeryBad")).toBe(false);
+})
